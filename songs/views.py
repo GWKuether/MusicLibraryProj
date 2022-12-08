1df936cf5f2bcca9077039e5d410498b349f529a
George Kuether <gwkuether@gmail.com>
2022-12-08 11:19:33 -0600

    Updated Project URLs + Created View Function + Created Songs App URLs File

--- songs/views.py
+++ songs/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET'])
+def songs_list(request):
+
+    return Response('ok')
