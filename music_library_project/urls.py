b2b40432d7f6e4b675fa6c3250f1e54bb6e3dc0d
George Kuether <gwkuether@gmail.com>
2022-12-08 12:52:14 -0600

    Created API Views for Get and Post Requests

--- music_library_project/urls.py
+++ music_library_project/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/songs/', include('songs.urls')),
+    path('api/music/', include('songs.urls')),
 ]
--- songs/views.py
+++ songs/views.py
@@ -1,7 +1,19 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SongSerializer
+from .models import Song
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def songs_list(request):
 
-    return Response('ok')
+    if request.method == 'GET':
+        songs = Song.objects.all()
+        serializer = SongSerializer(songs, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = SongSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
