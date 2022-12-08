7d91b9db615df801414ceeac6a748b4ebc72adb1
George Kuether <gwkuether@gmail.com>
2022-12-08 10:55:42 -0600

    Created Songs Model for Songs App + Migrated Model

--- songs/apps.py
+++ songs/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MusicConfig(AppConfig):
+class SongsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'music'
+    name = 'songs'
---
+++ songs/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Song(models.Model):
+    title = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255)
+    album = models.CharField(max_length=255)
+    release_date = models.CharField(max_length=255)
+    genre = models.CharField(max_length=255)
