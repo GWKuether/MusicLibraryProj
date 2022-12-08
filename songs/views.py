9e57aaf30fc75b827ca01eee8890ddeb17e5b6ba
George Kuether <gwkuether@gmail.com>
2022-12-08 13:29:07 -0600

    Completed Delete Functionality for Song View File

--- songs/views.py
+++ songs/views.py
@@ -18,7 +18,7 @@ def songs_list(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(['GET', 'PUT'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def song_detail(request, pk):
     song = get_object_or_404(Song, pk=pk)
     if request.method == 'GET':
@@ -29,5 +29,8 @@ def song_detail(request, pk):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+    elif request.method == 'DELETE':
+        song.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
